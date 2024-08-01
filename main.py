b34c141eecf625546f46cfa5009cf829e0d0a822
BBFaiL <bbfail.official@gmail.com>
2024-08-01 14:26:59 +0300

    Add requirements.txt, restore names.py, add support another site, bug fixes and much more

--- main.py
+++ main.py
@@ -1,24 +1,26 @@
 import time
-import pyautogui
 import getpass
-import os
-from names import search_tab, first_response, view_3D, copy_button
+import pyperclip
+from names import search_tab, first_response, search_tab_gta_object_xyz, first_response_gta_object_xyz
 
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-from PIL import ImageGrab, Image
-from io import BytesIO
-import win32clipboard
 
 
+user_check_site = int(input('Укажите сайт, через который вы хотите работать, 1 - plebmasters; 2 - gta-objects (рекомендуется): '))
 input_object = str(input('Укажите название объекта: '))
-User = getpass.getuser()
 
-driver = webdriver.Chrome()
-wait = WebDriverWait(driver, 60)
+
+options = webdriver.ChromeOptions()
+options.add_argument("--mute-audio")
+options.add_argument("--headless=new")
+
+
+driver = webdriver.Chrome(options=options)
+wait = WebDriverWait(driver, 120)
 
 
 def plebmasters(input_object):
@@ -27,63 +29,70 @@ def plebmasters(input_object):
     wait.until(EC.presence_of_element_located((By. XPATH, search_tab)))
 
     search = driver.find_element(By. XPATH, search_tab)
-    search.click()
     search.clear()
     search.send_keys(f'{input_object}')
     search.send_keys(Keys.ENTER)
+    print('Поиск завершен')
     wait.until(EC.visibility_of_element_located((By. XPATH, first_response)))
 
     first_layout = driver.find_element(By.XPATH, first_response)
     first_layout.click()
-    # wait.until(EC.visibility_of_element_located((By.XPATH, view_3D)))
+    print('Открыт объект')
+    wait.until(EC.visibility_of_element_located((By.CSS_SELECTOR, ".normal > img:nth-child(2)")))
 
-    # view = driver.find_element(By. XPATH, view_3D)
-    # view.click()
-    time.sleep(4.5)
-
-
-def screenshot(input_object):
-    screenshot = ImageGrab.grab(bbox=(790, 400, 1528, 800))
-    try:
-        screenshot.save(rf"C:\Users\{User}\Desktop\{input_object}.png")
-        screenshot.close()
-    except:
-        pass
+    img_element = driver.find_element(By.CSS_SELECTOR, ".normal > img:nth-child(2)")
+    img_url = img_element.get_attribute("src")
+    print(f"URL изображения: {img_url}")
+    return img_url
 
 
-def send_to_clipboard(clip_type, data):
-    win32clipboard.OpenClipboard()
-    win32clipboard.EmptyClipboard()
-    win32clipboard.SetClipboardData(clip_type, data)
-    win32clipboard.CloseClipboard()
+def gta_objects_xyz(input_object):
+    driver.maximize_window()
+    driver.get('https://gta-objects.xyz/objects')
+    wait.until(EC.presence_of_element_located((By. XPATH, search_tab_gta_object_xyz)))
 
-def image_to_clipboard():
-    filepath = rf"C:\Users\{User}\Desktop\{input_object}.png"
-    image = Image.open(filepath)
+    search = driver.find_element(By. XPATH, search_tab_gta_object_xyz)
+    search.clear()
+    search.send_keys(f'{input_object}')
+    search.send_keys(Keys.ENTER)
+    print('Поиск завершен')
+    wait.until(EC.presence_of_element_located((By.XPATH, first_response_gta_object_xyz)))
 
-    output = BytesIO()
-    image.convert("RGB").save(output, "BMP")
-    data = output.getvalue()[14:]
-    output.close()
+    first_layout = driver.find_element(By.XPATH, first_response_gta_object_xyz)
+    first_layout.click()
+    print('Открыт объект')
+    wait.until(EC.presence_of_element_located((By.ID, "objectImage")))
 
-    send_to_clipboard(win32clipboard.CF_DIB, data)
+    img_element = driver.find_element(By.ID, "objectImage")
+    img_url = img_element.get_attribute("src")
+    print(f"URL изображения: {img_url}")
+    return img_url
 
 
-def visionbot():
+def visionbot(img_url):
     driver.get('https://visionbot.ru/')
-    time.sleep(1.5)
-    pyautogui.hotkey('ctrl', 'v')
+    time.sleep(0.1)
+
+    paste_url = driver.find_element(By.XPATH, '//*[@id="userlink"]')
+    paste_url.send_keys(img_url)
+    paste_url.send_keys(Keys.ENTER)
+
+    print('Вставлено')
     wait.until(EC.presence_of_element_located((By. ID, 'success1')))
 
-    copy_text = driver.find_element(By. XPATH, copy_button)
-    copy_text.click()
+    success_element = driver.find_element(By.ID, 'success1')
+    text = success_element.text
+    pyperclip.copy(text)
+
+    print(text)
 
 
 def main(input_object):
-    plebmasters(input_object)
-    screenshot(input_object)
-    image_to_clipboard()
-    visionbot()
+    if user_check_site == 1:
+        img_url = plebmasters(input_object)
+    else:
+        img_url = gta_objects_xyz(input_object)
+    visionbot(img_url)
     driver.close()
     driver.quit()
 
--- names.py
+++ names.py
@@ -1,5 +1,7 @@
 search_tab = '//*[@id="input-8"]'
 first_response = '/html/body/div[1]/div/div/div/main/div/div/div[3]/div/div/div/div[2]/div[2]/div[1]/div/div/div[2]/div[3]'
-view_3D = '/html/body/div[3]/div[6]/div[2]/div/div/div[3]/div/div[2]/div/div/div[2]/div/div[1]/div[1]/div/div/button[2]'
+
+search_tab_gta_object_xyz = '/html/body/section[2]/div[1]/div[2]/div/form/p[1]/input'
+first_response_gta_object_xyz = '/html/body/section[2]/div[1]/div/div/div/div'
 
 copy_button = '//*[@id="copybtn"]'
