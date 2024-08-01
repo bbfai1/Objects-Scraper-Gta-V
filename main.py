2aca8fe34d9230b8a96c173c20687ce1988c6d0e
BBFaiL <bbfail.official@gmail.com>
2024-08-01 11:07:47 +0300

    Add files

--- main.py
+++ main.py
@@ -1,44 +1,43 @@
 import time
-from names import search_button, object_preview, model_3d, idk_button, User
+import pyautogui
+from names import User, search_tab, first_response, view_3D, layout, copy_button
 
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-from PIL import ImageGrab
+from PIL import ImageGrab, Image
+from io import BytesIO
+import win32clipboard
 
 
-options = webdriver.ChromeOptions()
-options.add_argument("--mute-audio")
+input_object = str(input('Укажите название объекта: '))
 
 
-driver = webdriver.Chrome(options=options)
-wait = WebDriverWait(driver, 15)
+driver = webdriver.Chrome()
+wait = WebDriverWait(driver, 60)
 
 
-def plebmasters():
-    input_object = input('Укажите название объекта: ')
-
+def plebmasters(input_object):
     driver.maximize_window()
     driver.get('https://forge.plebmasters.de/')
-    wait.until(EC.presence_of_element_located((By. XPATH, '//*[@id="input-8"]')))
+    wait.until(EC.presence_of_element_located((By. XPATH, search_tab)))
 
-    search = driver.find_element(By. XPATH, '//*[@id="input-8"]')
+    search = driver.find_element(By. XPATH, search_tab)
+    search.click()
     search.clear()
-    search.send_keys(input_object)
+    search.send_keys(f'{input_object}')
     search.send_keys(Keys.ENTER)
-    wait.until(EC.presence_of_element_located((By. XPATH, search_button)))
+    wait.until(EC.visibility_of_element_located((By. XPATH, first_response)))
 
-    photo_pre_view = driver.find_element(By. XPATH, object_preview)
-    photo_pre_view.click()
-    wait.until(EC.presence_of_element_located((By. XPATH, model_3d)))
+    first_layout = driver.find_element(By.XPATH, first_response)
+    first_layout.click()
+    wait.until(EC.visibility_of_element_located((By.XPATH, view_3D)))
 
-    photo_view = driver.find_element(By. XPATH, idk_button)
-    photo_view.click()
-    time.sleep(4)
-
-    return input_object
+    view = driver.find_element(By. XPATH, view_3D)
+    view.click()
+    time.sleep(4.5)
 
 
 def screenshot(input_object):
@@ -50,10 +49,40 @@ def screenshot(input_object):
         pass
 
 
-def main():
-    input_object = plebmasters()
+def send_to_clipboard(clip_type, data):
+    win32clipboard.OpenClipboard()
+    win32clipboard.EmptyClipboard()
+    win32clipboard.SetClipboardData(clip_type, data)
+    win32clipboard.CloseClipboard()
+
+def image_to_clipboard():
+    filepath = rf"C:\Users\{User}\Desktop\{input_object}.png"
+    image = Image.open(filepath)
+
+    output = BytesIO()
+    image.convert("RGB").save(output, "BMP")
+    data = output.getvalue()[14:]
+    output.close()
+
+    send_to_clipboard(win32clipboard.CF_DIB, data)
+
+
+def visionbot():
+    driver.get('https://visionbot.ru/')
+    time.sleep(3)
+    pyautogui.hotkey('ctrl', 'v')
+    wait.until(EC.presence_of_element_located((By. ID, 'success1')))
+
+    copy_text = driver.find_element(By. XPATH, copy_button)
+    copy_text.click()
+
+
+def main(input_object):
+    plebmasters(input_object)
     screenshot(input_object)
+    image_to_clipboard()
+    visionbot()
 
 
 if __name__ == '__main__':
-    main()
+    main(input_object)
--- names.py
+++ names.py
@@ -1,6 +1,8 @@
-search_button = '/html/body/div[1]/div/div/div/main/div/div/div[3]/div/div/div/div[2]/div[2]/div[1]/div/div/div[2]/div[3]'
-object_preview = '/html/body/div[1]/div/div/div/main/div/div/div[3]/div/div/div/div[2]/div[2]/div[1]/div/div/div[2]/div[3]'
-model_3d = '/html/body/div[3]/div[6]/div[2]/div/div/div[3]/div/div[2]/div/div/div[2]/div/div[1]/div[1]/div/div/button[2]/span[3]'
-idk_button = '/html/body/div[3]/div[6]/div[2]/div/div/div[3]/div/div[2]/div/div/div[2]/div/div[1]/div[1]/div/div/button[2]/span[3]'
+search_tab = '//*[@id="input-8"]'
+first_response = '/html/body/div[1]/div/div/div/main/div/div/div[3]/div/div/div/div[2]/div[2]/div[1]/div/div/div[2]/div[3]'
+view_3D = '/html/body/div[3]/div[6]/div[2]/div/div/div[3]/div/div[2]/div/div/div[2]/div/div[1]/div[1]/div/div/button[2]'
+
+layout = '/html/body/article/div/p/button'
+copy_button = '//*[@id="copybtn"]'
 
 User = 'BBFaiL'
