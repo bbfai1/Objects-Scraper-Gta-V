15ccf4c3a4169a72ad1de2e7fca63627458f7d0a
BBFaiL <bbfail.official@gmail.com>
2024-08-03 17:00:45 +0300

    оптимизация кода

--- chromedriver.py
+++ chromedriver.py
@@ -1,11 +1,8 @@
 from selenium import webdriver
 
-# Путь до расширения, блокирующего рекламу, которое написано под браузеры использующие Chromium
-path = r'C:\Users\BBFaiL\PycharmProjects\Objects-Scraper-Gta-V\uBlock-Origin-Chrome.crx'
-
 # Добавление расширения, отключения звука и скрытие окна браузера
 options = webdriver.ChromeOptions()
-options.add_extension(path)
+options.add_extension('uBlock-Origin-Chrome.crx')
 options.add_argument("--mute-audio")
 options.add_argument("--headless=new")
 
